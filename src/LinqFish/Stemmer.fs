b29c7b939c81f71b665a636ed607bc72e428d32a
brettm42 <brettm42@gmail.com>
2015-11-10 17:29:40 -0800

    fix to compile

--- src/LinqFish/Stemmer.fs
+++ src/LinqFish/Stemmer.fs
@@ -9,14 +9,14 @@ module Stemmer =
     let public Affixes = 
         [| "es", "ed", "ing", "en", "ness", "ly", "able", "tion" |]
 
-    let isAffix s =
-        Affixes |> Seq.iter(
-            if (Regex.IsMatch(s, m)) then Regex.Replace(s, m, String.Empty)
-            else String.Empty)
+//    let isAffix s =
+//        Affixes |> Seq.iter(
+//            if (Regex.IsMatch(s, m)) then Regex.Replace(s, m, String.Empty)
+//            else String.Empty)
 
     let public GetStem(v, n) =
-        function
-        | isAffix(v) -> printfn "found an affix! %s" v
+        match v with
+        | Affixes -> printfn "found an affix! %s" v
         | _ -> printfn "null"
                 
     let Locale = Globalization.CultureInfo.GetCultureInfo("en-US")
