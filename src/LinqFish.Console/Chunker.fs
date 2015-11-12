d4f736c4e34468e79a1c58cd615ac69e5b94b323
brettm42 <brettm42@gmail.com>
2015-11-12 11:30:29 -0800

    added string ending catches

--- src/LinqFish.Console/Chunker.fs
+++ src/LinqFish.Console/Chunker.fs
@@ -24,7 +24,9 @@ module Chunker =
     let public GetBigrams (args : string) =
         let arr = args.Split(' ')
         [| for a in 0 .. 1 .. (arr.Length - 2) do
-            yield (arr.[a], arr.[a + 1]) |]
+            if a = 0 then yield ("", arr.[a + 1])
+            else if a = arr.Length - 2 then yield (arr.[a], "")
+            else yield (arr.[a], arr.[a + 1]) |]
 
     let public Select(t, p) =
         match t with
