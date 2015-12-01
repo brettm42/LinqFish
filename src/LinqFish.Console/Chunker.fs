052c52d2ba6a7c0014278d8938eb20ba5dd1759c
brettm42 <brettm42@gmail.com>
2015-12-01 07:40:07 -0800

    updated bigram chunker

--- src/LinqFish.Console/Chunker.fs
+++ src/LinqFish.Console/Chunker.fs
@@ -8,37 +8,32 @@ module Chunker =
     open System.Text.RegularExpressions
     
     let public GetBigrams (args : string) =
-        let arr = ("^ " + args + " $").Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-        let len = arr.Length - 1
+        let arr = ("^ " + args.Trim() + " $").Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+        let len = arr.Length - 2
         [| for a in 0 .. 1 .. len do
-            if a = len then yield (arr.[a], "")
-            else yield (arr.[a], arr.[a + 1]) |]
+            yield (arr.[a], arr.[a + 1]) |]
                                                  
     let public GetBigramsSep (args : string, separator : char) =
-        let arr =("^" + args + "$").Split(separator)
-        let len = arr.Length - 1
+        let arr = ("^" + separator.ToString() + args + separator.ToString() + "$").Split(separator)
+        let len = arr.Length - 2
         [| for a in 0 .. 1 .. len do
-            match a with
-            | len -> yield (arr.[a], "")
-            | _ -> yield (arr.[a], arr.[a + 1]) |]
+            yield (arr.[a], arr.[a + 1]) |]
 
     let public GetTrigrams(args : string) =
         let arr = ("^ " + args + " $").Split(' ')
         let len = arr.Length - 2
         [| for a in 0 .. 2 .. len do 
-            match a with
-            | 0 -> yield ("", arr.[a], arr.[a + 1])
-            | len -> yield (arr.[a - 1], arr.[a], "")
-            | _ -> yield (arr.[a - 1], arr.[a], arr.[a + 1]) |]
+            if a = 0 then yield ("", arr.[a], arr.[a + 1])
+            else if a = len then yield (arr.[a - 1], arr.[a], "")
+            else yield (arr.[a - 1], arr.[a], arr.[a + 1]) |]
 
     let public GetTrigramsSep(args : string, separator : char) =
-        let arr = ("^ " + args + " $").Split(separator)
+        let arr = ("^" + separator.ToString() + args + separator.ToString() + "$").Split(separator)
         let len = arr.Length - 2
         [| for a in 0 .. 2 .. len do 
-            match a with
-            | 0 -> yield ("", arr.[a], arr.[a + 1])
-            | len -> yield (arr.[a - 1], arr.[a], "")
-            | _ -> yield (arr.[a - 1], arr.[a], arr.[a + 1]) |]
+            if a = 0 then yield ("", arr.[a], arr.[a + 1])
+            else if a = len then yield (arr.[a - 1], arr.[a], "")
+            else yield (arr.[a - 1], arr.[a], arr.[a + 1]) |]
 
     let public SelectSeq grams =
         seq { for gram in grams do
