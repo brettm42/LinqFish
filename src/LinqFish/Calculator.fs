2cf01b625d1a28a136c4c8a37a78cdbdaa6b5dc6
brettm42 <brettm42@gmail.com>
2015-12-10 11:28:57 -0800

    small fixes, added Calculator skeleton class

---
+++ src/LinqFish/Calculator.fs
@@ -0,0 +1,15 @@
+﻿namespace LinqFish
+
+[<AutoOpen>]
+module Calculator =
+    open System.Globalization
+    open System.Text
+    open System.Text.RegularExpressions
+
+    let public GetDistribution ngrams =
+        0
+
+    let public GetWordCounts ngrams = 
+        0
+
+    let Locale = CultureInfo.GetCultureInfo("en-US")
