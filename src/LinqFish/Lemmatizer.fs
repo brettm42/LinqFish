ac59ae119a001111d002ef6d43d2665ab4d98b2b
brettm42 <brettm42@gmail.com>
2016-01-03 13:26:51 -0800

    fixed variable consistency

--- src/LinqFish/Lemmatizer.fs
+++ src/LinqFish/Lemmatizer.fs
@@ -6,13 +6,13 @@ module Lemmatizer =
     open System.Text
     open System.Text.RegularExpressions
 
-    let public DictionaryENUS =
+    let public Dictionary_enUS =
         seq [ "test"; "get"; "this";  ]
 
     let public GetLemma(v, n) =
         let length = String.length(v : string) - 1
         [| for c = 0 to length do
-            let stem = Seq.tryFind (fun sl -> sl = v.[0..c]) DictionaryENUS
+            let stem = Seq.tryFind (fun sl -> sl = v.[0..c]) Dictionary_enUS
             if stem.IsSome then 
                 yield Regex.Replace(v, stem.Value.ToString(), "") |]
 
